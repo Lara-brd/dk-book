04d0928216b501438b7327bab45d96b171cee3d8
LaraSanz <lara@veosat.com>
2026-04-11 12:31:46 +0200

    Feat: custom menu

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,2 +1,22 @@
 
 export const IMAGE_URL = 'assets/icons';
+
+
+
+import { MatMenuDefaultOptions } from '@angular/material/menu';
+
+export const MENU_DEFAULT_OPTIONS: MatMenuDefaultOptions = {
+  overlapTrigger: true,
+  xPosition: 'before',
+  yPosition: 'above',
+  backdropClass: 'menu-backdrop',
+  hasBackdrop: true,
+};
+
+// angular material por defecto usa
+// {
+//   overlapTrigger: false,
+//   xPosition: 'after',
+//   yPosition: 'below',
+//   hasBackdrop: true,
+// }
--- src/app/ui/dashboard-header/dashboard-header.ts
+++ src/app/ui/dashboard-header/dashboard-header.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
+import { MAT_MENU_DEFAULT_OPTIONS, MatMenuModule } from '@angular/material/menu';
+import { MENU_DEFAULT_OPTIONS } from '../../constants/constants';
 
 @Component({
   selector: 'app-dashboard-header',
@@ -10,7 +11,24 @@ import { MatMenuModule } from '@angular/material/menu';
     MatIconModule,
     MatMenuModule
   ],
+
+  providers: [
+    {
+      provide: MAT_MENU_DEFAULT_OPTIONS,
+      useValue: MENU_DEFAULT_OPTIONS,
+    },
+  ],
   templateUrl: './dashboard-header.html',
   styleUrl: './dashboard-header.scss',
 })
-export class DashboardHeader {}
+export class DashboardHeader {
+
+  // si quisiera en unos determinados menus puedo crear una constante y reutilizarla
+  readonly menuConfig = {
+    overlapTrigger: false,
+    xPosition: 'before' as const,
+    yPosition: 'above' as const,
+    hasBackdrop: true,
+  };
+
+}
